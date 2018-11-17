cd03b70a365285ca6b73cd0645b99d9c1d255b31
A1 <A1@192.168.0.2>
2018-11-17 18:17:25 +0800

    更新：     添加后台模块，实现页面搭建、路由设置、数据表与模型；     实现了后台登录功能；     实现了后台登出功能；
    修改：
        将后台的路由从WebRoute中单独提取出来，并在ServiceRouteProvider中进行注册。
    
    修复：
    
    已知问题：
    1.图片上传；
    2.文章创建与编辑页面的编辑器不能正常显示；

--- app/Mariadb/Backend/BackendUser.php
+++ app/Mariadb/Backend/BackendUser.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class BackendUser extends Authenticatable {
 
     protected $table = 'backend_users';
+    protected $rememberTokenName = '';
     protected $fillable = ['name', 'password'];
     protected $primaryKey = 'id';
 
