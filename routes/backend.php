97aa5da856e57a20330757f98156a0c2be908acd
A1 <A1@192.168.0.2>
2018-11-30 20:24:10 +0800

    更新：     完成后台权限、角色的数据表迁移与模型构建；     完成后台管理的权限与角色路由；
    修改：
        分离前台控制器到独立目录；
        为前台、后台和web注册独立的中间件；
    
    修复：
        由scout目录不可访问导致的搜索模块功能异常；
        修复一些前端模板的引用路径问题；
    
    已知问题：
        1. 图片上传；
        2. 文章创建与编辑页面的编辑器不能正常显示；
        3. 文章审核页面js脚本不工作，导致审核失败。需要关注后台模型查询数据时，对id的处理；

--- routes/backend.php
+++ routes/backend.php
@@ -5,12 +5,15 @@
  */
 Route::prefix('backend')->group(function () {
 
-    // 登录页面
+    /**
+     * 后台登录、登出
+     * 
+     *   登录页面
+     *   登录表单数据处理，登录行为
+     *   登出行为
+     */
     Route::get('login', 'LoginController@show');
-    // 登录表单数据处理，登录行为
     Route::post('login', 'LoginController@login');
-
-    // 登出行为
     Route::get('logout', 'LoginController@logout');
 
     Route::group(['middleware' => 'auth:backend'], function() {
@@ -22,24 +25,50 @@ Route::prefix('backend')->group(function () {
 
         /**
          * 后台管理人员模块
+         * 
+         *   管理人员列表页
+         *   添加管理人员
+         *   存储添加的管理人员
+         *   用户的角色与职位管理页面
+         *   用户的角色与职位管理逻辑
          */
         Route::get('users', 'UserController@index');
-        // 添加管理人员
         Route::get('users/create', 'UserController@create');
-        // 存储添加的管理人员
         Route::post('users/store', 'UserController@store');
+        Route::get('users/role/{user}', 'UserController@role');
+        Route::post('users/role/{user}', 'UserController@storeRole');
 
         /**
          * 角色与职位管理模块
+         * 
+         *   角色列表页
+         *   角色创建页
+         *   角色创建逻辑
+         *   角色的权限详情页面
+         *   角色的权限管理逻辑
          */
-        Route::get();
+        Route::get('roles', 'RoleController@index');
+        Route::get('roles/create', 'RoleController@create');
+        Route::post('roles/create', 'RoleController@store');
+        Route::get('roles/permission', 'RoleController@permission');
+        Route::post('roles/permission/{role}', 'RoleController@storePeimission');
         
         /**
          * 权限管理模块
+         * 
+         *   权限列表页面
+         *   权限创建页面
+         *   权限创建逻辑
          */
+        Route::get('permissions', 'PermissionController@index');
+        Route::get('permissions/create', 'PermissionController@create');
+        Route::post('permissions', 'PermissionController@store');
         
         /**
          * 文章审核模块
+         * 
+         *   待审核文章列表页
+         *   文章审核逻辑
          */
         Route::get('posts', 'PostController@index');
         Route::post('posts/status', 'PostController@status');
