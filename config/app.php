7f571291b15932fe1456711f2b334869b6c0719c
passpos <passpos@outlook.com>
2020-10-28 03:41:25 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
       |--------------------------------------------------------------------------
       | Application Name
@@ -13,8 +12,7 @@ return [
       |
      */
 
-    'name' => env('APP_NAME', 'Laravel'),
-
+    'name'            => env('APP_NAME', 'Laravel'),
     /*
       |--------------------------------------------------------------------------
       | Application Environment
@@ -25,9 +23,7 @@ return [
       | services the application utilizes. Set this in your ".env" file.
       |
      */
-
-    'env' => env('APP_ENV', 'production'),
-
+    'env'             => env('APP_ENV', 'production'),
     /*
       |--------------------------------------------------------------------------
       | Application Debug Mode
@@ -38,8 +34,7 @@ return [
       | application. If disabled, a simple generic error page is shown.
       |
      */
-
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug'           => (bool) env('APP_DEBUG', false),
     /*
       |--------------------------------------------------------------------------
       | Application URL
@@ -50,10 +45,8 @@ return [
       | your application so that it is used when running Artisan tasks.
       |
      */
-
-    'url' => env('APP_URL', 'http://localhost'),
-
-    'asset_url' => env('ASSET_URL', null),
+    'url'             => env('APP_URL', 'http://localhost'),
+    'asset_url'       => env('ASSET_URL', null),
     /*
       |--------------------------------------------------------------------------
       | Application Timezone
@@ -64,7 +57,7 @@ return [
       | ahead and set this to a sensible default for you out of the box.
       |
      */
-    'timezone' => 'Asia/Shanghai',
+    'timezone'        => 'Asia/Shanghai',
     /*
       |--------------------------------------------------------------------------
       | Application Locale Configuration
@@ -75,7 +68,7 @@ return [
       | to any of the locales which will be supported by the application.
       |
      */
-    'locale' => 'cn',
+    'locale'          => 'cn',
     /*
       |--------------------------------------------------------------------------
       | Application Fallback Locale
@@ -86,20 +79,18 @@ return [
       | the language folders that are provided through your application.
       |
      */
-
     'fallback_locale' => 'en',
-
     /*
-    |--------------------------------------------------------------------------
-    | Faker Locale
-    |--------------------------------------------------------------------------
-    |
-    | This locale will be used by the Faker PHP library when generating fake
-    | data for your database seeds. For example, this will be used to get
-    | localized telephone numbers, street address information and more.
-    |
-    */
-    'faker_locale' => 'en_US',
+      |--------------------------------------------------------------------------
+      | Faker Locale
+      |--------------------------------------------------------------------------
+      |
+      | This locale will be used by the Faker PHP library when generating fake
+      | data for your database seeds. For example, this will be used to get
+      | localized telephone numbers, street address information and more.
+      |
+     */
+    'faker_locale'    => 'en_US',
     /*
       |--------------------------------------------------------------------------
       | Encryption Key
@@ -110,11 +101,8 @@ return [
       | will not be safe. Please do this before deploying an application!
       |
      */
-
-    'key' => env('APP_KEY'),
-
-    'cipher' => 'AES-256-CBC',
-
+    'key'             => env('APP_KEY'),
+    'cipher'          => 'AES-256-CBC',
     /*
       |--------------------------------------------------------------------------
       | Autoloaded Service Providers
@@ -125,9 +113,7 @@ return [
       | this array to grant expanded functionality to your applications.
       |
      */
-
-    'providers' => [
-
+    'providers'       => [
         /*
          * Laravel Framework Service Providers...
          */
@@ -153,11 +139,12 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
-
+        Laravel\Scout\ScoutServiceProvider::class,
+        Tamayo\LaravelScoutElastic\LaravelScoutElasticProvider::class,
+        
         /*
          * Application Service Providers...
          */
@@ -166,9 +153,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
-
     /*
       |--------------------------------------------------------------------------
       | Class Aliases
@@ -179,46 +164,42 @@ return [
       | the aliases are "lazy" loaded so they don't hinder performance.
       |
      */
-
-    'aliases' => [
-
-        'App' => Illuminate\Support\Facades\App::class,
+    'aliases'         => [
+        'App'          => Illuminate\Support\Facades\App::class,
         'Arr'          => Illuminate\Support\Arr::class,
-        'Artisan' => Illuminate\Support\Facades\Artisan::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => Illuminate\Support\Facades\Bus::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'Cookie' => Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => Illuminate\Support\Facades\Crypt::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
-        'Event' => Illuminate\Support\Facades\Event::class,
-        'File' => Illuminate\Support\Facades\File::class,
-        'Gate' => Illuminate\Support\Facades\Gate::class,
-        'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Artisan'      => Illuminate\Support\Facades\Artisan::class,
+        'Auth'         => Illuminate\Support\Facades\Auth::class,
+        'Blade'        => Illuminate\Support\Facades\Blade::class,
+        'Broadcast'    => Illuminate\Support\Facades\Broadcast::class,
+        'Bus'          => Illuminate\Support\Facades\Bus::class,
+        'Cache'        => Illuminate\Support\Facades\Cache::class,
+        'Config'       => Illuminate\Support\Facades\Config::class,
+        'Cookie'       => Illuminate\Support\Facades\Cookie::class,
+        'Crypt'        => Illuminate\Support\Facades\Crypt::class,
+        'DB'           => Illuminate\Support\Facades\DB::class,
+        'Eloquent'     => Illuminate\Database\Eloquent\Model::class,
+        'Event'        => Illuminate\Support\Facades\Event::class,
+        'File'         => Illuminate\Support\Facades\File::class,
+        'Gate'         => Illuminate\Support\Facades\Gate::class,
+        'Hash'         => Illuminate\Support\Facades\Hash::class,
         'Http'         => Illuminate\Support\Facades\Http::class,
-        'Lang' => Illuminate\Support\Facades\Lang::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Lang'         => Illuminate\Support\Facades\Lang::class,
+        'Log'          => Illuminate\Support\Facades\Log::class,
+        'Mail'         => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
-        'Password' => Illuminate\Support\Facades\Password::class,
-        'Queue' => Illuminate\Support\Facades\Queue::class,
-        'Redirect' => Illuminate\Support\Facades\Redirect::class,
-        'Redis' => Illuminate\Support\Facades\Redis::class,
-        'Request' => Illuminate\Support\Facades\Request::class,
-        'Response' => Illuminate\Support\Facades\Response::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'Storage' => Illuminate\Support\Facades\Storage::class,
+        'Password'     => Illuminate\Support\Facades\Password::class,
+        'Queue'        => Illuminate\Support\Facades\Queue::class,
+        'Redirect'     => Illuminate\Support\Facades\Redirect::class,
+        'Redis'        => Illuminate\Support\Facades\Redis::class,
+        'Request'      => Illuminate\Support\Facades\Request::class,
+        'Response'     => Illuminate\Support\Facades\Response::class,
+        'Route'        => Illuminate\Support\Facades\Route::class,
+        'Schema'       => Illuminate\Support\Facades\Schema::class,
+        'Session'      => Illuminate\Support\Facades\Session::class,
+        'Storage'      => Illuminate\Support\Facades\Storage::class,
         'Str'          => Illuminate\Support\Str::class,
-        'URL' => Illuminate\Support\Facades\URL::class,
-        'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-
+        'URL'          => Illuminate\Support\Facades\URL::class,
+        'Validator'    => Illuminate\Support\Facades\Validator::class,
+        'View'         => Illuminate\Support\Facades\View::class,
     ],
-
 ];
